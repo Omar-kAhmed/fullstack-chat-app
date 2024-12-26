2c339d3ec1b058980c3ade625850f6e296ef543d
Omar Ahmed <omarkhaledahmedov7@gmail.com>
2024-12-26 14:35:15 +0100

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-backend-v42m.onrender.com" : "/api",
   withCredentials: true,
 });
